8d971028ed936134c894da400edd5908e808d05e
zesik1 <kongaronis@gmail.com>
2022-10-22 12:21:32 +0300

    read grade from student id

--- src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -20,7 +20,7 @@ public class GradeServiceImpl implements GradeService {
     
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
-        return null;
+        return gradeRepository.findByStudentId(studentId);
     }
 
     @Override
