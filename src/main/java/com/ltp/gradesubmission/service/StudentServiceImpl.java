46cdd272b063761a0180ef8c6fec3e402cf4f045
zesik1 <overdrivemono@gmail.com>
2022-10-22 19:47:15 +0300

    cascade student with database grades

--- src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -1,9 +1,8 @@
 package com.ltp.gradesubmission.service;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.ltp.gradesubmission.entity.Grade;
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.repository.StudentRepository;
 
@@ -26,14 +25,12 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public void deleteStudent(Long id) {
-        studentRepository.deleteById(id);        
+        studentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> getStudents() {
-        return (List<Student>)studentRepository.findAll();
+        return (List<Student>) studentRepository.findAll();
     }
-
-
 
 }
